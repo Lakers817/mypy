8bbbfa470ae04606eed2512116572e74a8506b7a
Lakers817 <airmango817@gmail.com>
2017-09-13 17:17:14 +0000

    new file

---
+++ test.py
@@ -0,0 +1,19 @@
+from cassandra.cluster import Cluster
+import sys
+import os
+from os import environ
+
+ty = os.getenv('cass')
+
+print(ty)
+cluster = Cluster([ty])
+
+keyspace = 'northwind'
+
+connection = cluster.connect(keyspace)
+
+first_orders = connection.execute('select * from orders limit 5')
+
+for order in first_orders:
+    print("Orders %d - To city %s" %(order.ordersid, order.shipcity))
+

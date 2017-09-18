ff88de2a2cfea77f806997e5d2418974c6897bc5
Lakers817 <airmango817@gmail.com>
2017-09-18 19:06:58 +0000

    added

--- files/test.py
+++ files/test.py
@@ -1,2 +1,25 @@
+from cassandra.cluster import Cluster
+import sys
+import os
+from os import environ
+import unicodedata
 
-python version
+#code start now
+
+ty = os.getenv('cass')
+
+print(ty)
+
+cluster = Cluster([ty])
+
+keyspace = 'northwind'
+
+connection = cluster.connect(keyspace)
+
+first_orders = connection.execute('select * from orders limit 7')
+
+for order in first_orders:
+    print("Orders %d - To city %s" %(order.ordersid, order.shipcity.encode('utf-8')))
+~                                                                                                                                                                        
+~                                                                                                                                                                        
+~                                                                                          

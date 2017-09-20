2e1ba79ff120ee7db3ecb535ee00d58253a652a0
Lakers817 <airmango817@gmail.com>
2017-09-20 17:54:54 +0000

    added

--- scripts/start_python.sh
+++ scripts/start_python.sh
@@ -3,7 +3,8 @@
 
 cd /home/ubuntu/
 
-passy=$(sudo aws ssm get-parameters --region us-east-1 --names cass --with-decryption --query Parameters[0].Value)
+passy=$(aws ssm get-parameters --region us-east-1 --names cass --with-decryption --query Parameters[0].Value)
+
 passy=`echo $passy | sed -e 's/^"//' -e 's/"$//'`
 
 export passy=$passy

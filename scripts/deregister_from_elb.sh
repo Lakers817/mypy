be26ddb4123cce8b621cfc33fb4ed9d16ec3b2ad
Lakers817 <airmango817@gmail.com>
2017-09-23 19:02:05 +0000

    added ELB files

---
+++ scripts/deregister_from_elb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export AWS_DEFAULT_REGION=us-east-1
+
+
+instanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
+
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name lakers-server-ASG --health-check-type EC2
+
+
+aws elb deregister-instances-from-load-balancer --load-balancer-name CodeDeploy-Load-Balancer --instances $instanceID
+
+
+sleep 60
+
